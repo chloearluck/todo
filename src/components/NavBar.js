67d0762fd745657cf84f5a90c8c44ed7b8706a2b
Chloe Arluck <chloearluck@gmail.com>
2020-05-31 13:14:44 -0400

    React-Bootstapify the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 class NavBar extends Component {
   constructor() {
     super()
@@ -22,48 +25,29 @@ class NavBar extends Component {
 
   render() {
     return (
-      <div className="navbar navbar-light fixed-top navbar-expand-sm bg-light">
-        <div className="container"><a className="navbar-brand" href="/"><i className="fas fa-cube"></i>To Do App</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-          <div className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <Link to='/'>
-                <li className="nav-item"><p className="nav-link">Home</p></li>
-              </Link>
-              <Link to='/about'>
-                <li className="nav-item"><p className="nav-link">About</p></li>
-              </Link>
-            </ul>
-
-            <ul className="nav navbar-nav ml-auto">
-            { !this.props.isAuthenticated &&
-              <React.Fragment>
-              <Link to='/login'>
-                <li className="nav-item"><p className="nav-link">Login</p></li>
-              </Link>
-              <Link to='/signup'>
-                <li className="nav-item"><p className="nav-link">Signup</p></li>
-              </Link>
-              </React.Fragment>
-            }
-
-            { this.props.isAuthenticated &&
-              <React.Fragment>
-              <li className="nav-item"><p className="nav-link disabled">Hello, {this.props.email}</p></li>
-              <Link to='/account'>
-                <li className="nav-item"><p className="nav-link">Account</p></li>
-              </Link>
-              <Link to='/'>
-                <li className="nav-item"><p className="nav-link" onClick={this.logout}>Logout</p></li>
-              </Link>
-              </React.Fragment>
-            }
-
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
+      <Navbar bg="light" fixed="top" expand="sm">
+        <Navbar.Brand as={Link} to="/">My Planner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+          </Nav>
+          { this.props.isAuthenticated &&
+            <Nav className="ml-auto">
+              <Nav.Link disabled>{this.props.email}</Nav.Link>
+              <Nav.Link as={Link} to="/account">Account</Nav.Link>
+              <Nav.Link as={Link} to="/" onClick={this.logout}>Logout</Nav.Link>
+            </Nav>
+          }
+          { !this.props.isAuthenticated &&
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+            </Nav>
+          }
+        </Navbar.Collapse>
+      </Navbar>
+      );
   }
 }
 
